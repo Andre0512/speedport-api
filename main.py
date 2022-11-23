6cbb24e119e936cb079905c612736607955137b4
Andre Basche <andre.basche@gmail.com>
2022-11-24 00:51:48 +0100

    Add useful functions

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python
 import json
 import re
-import time
 from hashlib import sha256
-from pprint import pprint
+from json import JSONDecodeError
 
 import requests as requests
 from Crypto.Cipher import AES
-
-URL = "http://speedport.ip"
 
 
 class WlanDevice(dict):
@@ -18,18 +15,21 @@ class WlanDevice(dict):
 
 
 class Speedport:
-    def __init__(self):
+    def __init__(self, host="speedport.ip"):
         self._default_key = "cdc0cac1280b516e674f0057e4929bca84447cca8425007e33a88a5cf598a190"
         self._login_key = ""
-        self._httoken = ""
         self._cookies = {}
+        self._url = f"http://{host}"
 
-    def decode(self, data, key="", to_json=True):
+    def decode(self, data, key=""):
         key = key or self._default_key
         ciphertext_tag = bytes.fromhex(data)
         cipher = AES.new(bytes.fromhex(key), AES.MODE_CCM, bytes.fromhex(key)[:8])
         decrypted = cipher.decrypt_and_verify(ciphertext_tag[:-16], ciphertext_tag[-16:])
-        return json.loads(decrypted.decode()) if to_json else decrypted.decode()
+        try:
+            return self._simplify_response(json.loads(decrypted.decode()))
+        except JSONDecodeError:
+            return decrypted.decode()
 
     def encode(self, data, key=""):
         key = key or self._default_key
@@ -37,14 +37,24 @@ class Speedport:
         ciphertext, tag = cipher.encrypt_and_digest(data.encode())
         return ciphertext.hex() + tag.hex()
 
-    def get(self, path, key="", to_json=True):
-        response = requests.get(f"{URL}/{path}", cookies=self._cookies).text
-        return self.decode(response, key=key, to_json=to_json)
+    def get(self, path, key="", referer=""):
+        url = f"{self._url}/{path}"
+        kwargs = {"cookies": self._cookies}
+        if referer:
+            kwargs.update({"headers": {"Referer": f"{self._url}/{referer}"}})
+            url += f"?_tn={self._get_httoken(referer)}"
+        response = requests.get(url, **kwargs).text
+        return self.decode(response, key=key)
 
-    def post(self, path, data, key="", to_json=True):
-        data = self.encode(f"{data}&httoken={self._httoken}", key=key)
-        response = requests.post(f"{URL}/{path}", cookies=self._cookies, data=data).text
-        return self.decode(response, key=key, to_json=to_json)
+    def post(self, url, data, referer):
+        referer = f"{self._url}/{referer}"
+        data = self.encode(f"{data}&httoken={self._get_httoken(referer)}", key=self._login_key)
+        response = requests.post(url, cookies=self._cookies, headers={"Referer": referer}, data=data).text
+        return self.decode(response, key=self._login_key)
+
+    def _get_httoken(self, url):
+        response = requests.get(url, cookies=self._cookies)
+        return re.findall("_httoken = (\\d+)", response.text)[0]
 
     @staticmethod
     def _simplify_response(data):
@@ -59,34 +69,25 @@ class Speedport:
                 result[item["varid"]] = item["varvalue"]
         return result
 
-    def _get_httoken(self):
-        if not self._httoken:
-            response = requests.get(URL)
-            self._httoken = re.findall("_httoken = (\\d+)", response.text)[0]
-        return self._httoken
-
     def _get_login_key(self):
         if not self._login_key:
-            data = self.encode(f"getChallenge=1&httoken={self._get_httoken()}")
-            response = requests.post(f"{URL}/data/Login.json", data=data)
-            self._login_key = self._simplify_response(self.decode(response.text))['challenge']
+            self._login_key = self.post("data/Login.json", "getChallenge=1", "/")['challenge']
         return self._login_key
 
     def login(self, password):
-        if not self._cookies:
-            data = self.encode(
-                "showpw=0&password=" + sha256(f"{self._get_login_key()}:{password}".encode()).hexdigest())
-            response = requests.post(f"{URL}/data/Login.json", data=data)
-            # pprint(simplify_response(decode(response.text)))
+        if not self._login_key:
+            login_key = sha256(f"{self._get_login_key()}:{password}".encode()).hexdigest()
+            data = self.encode("showpw=0&password=" + login_key)
+            response = requests.post(f"data/Login.json", data=data)
             self._cookies = response.cookies
 
     @property
     def status(self):
-        return self._simplify_response(self.get("data/Status.json"))
+        return self.get("data/Status.json")
 
     @property
     def devices(self):
-        data = self._simplify_response(self.get("data/DeviceList.json"))
+        data = self.get("data/DeviceList.json")
         result = []
         devices = data.get("addmlandevice", []) + data.get("addmwlan5device", []) + data.get("addmwlandevice", [])
         for device in devices:
@@ -102,3 +103,36 @@ class Speedport:
             new["type"] = ["lan", "wlan", "wlan5"][int(device["mdevice_type"])]
             result.append(new)
         return result
+
+    @property
+    def ip_data(self):
+        referer = "html/content/internet/con_ipdata.html"
+        return self.get(f"data/IPData.json", referer=referer, key=self._login_key)
+
+    @property
+    def wps_state(self):
+        referer = "html/content/network/wlan_wps.html"
+        return self.get(f"data/WPSStatus.json", referer=referer)["wlan_wps_state"]
+
+    def _set_wifi(self, on=True, guest=False):
+        data = f"wlan_guest_active={int(on)}" if guest else f"use_wlan={int(on)}"
+        referer = f"html/content/network/wlan_{'guest' if guest else 'basic'}.html"
+        return self.post(f"data/{'WLANBasic' if guest else 'Modules'}.json", data, referer)
+
+    def wifi_on(self):
+        self._set_wifi(on=True, guest=False)
+
+    def wifi_off(self):
+        self._set_wifi(on=False, guest=False)
+
+    def wifi_guest_on(self):
+        self._set_wifi(on=True, guest=True)
+
+    def wifi_guest_off(self):
+        self._set_wifi(on=False, guest=True)
+
+    def wps_on(self):
+        self.post("data/WLANAccess.json", "wlan_add=on&wps_key=connect", "html/content/network/wlan_wps.html")
+
+    def reconnect(self):
+        self.post(f"data/Connect.json", "req_connect=reconnect", f"html/content/internet/con_ipdata.html")
